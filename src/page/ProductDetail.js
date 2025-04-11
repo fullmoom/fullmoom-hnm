0491050691a96ff29d98af1f9c5c1f6ec136cbac
fullmoom <wshong46@gmail.com>
2025-04-12 04:02:07 +0900

    Update ProductDetail.js

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const { id } = useParams();
   const getProductDetail = async () => {
     setLoading(true);
-    let url = `https://my-json-server.typicode.com/fullmoom/hnm-router-hw-1st/products/${id}`;
+    let url = `https://my-json-server.typicode.com/fullmoom/fullmoom-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setLoading(false);
