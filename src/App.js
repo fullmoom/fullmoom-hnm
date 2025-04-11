ea1ad698440eaf2c800999a86ddd73efd8340f90
fullmoom <wshong46@gmail.com>
2025-04-12 02:54:27 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import ProductAll from "./page/ProductAll";
-import Login from "./page/Login" ;
-import ProductDetail from "./page/ProductDetail" ;
-import Navbar from "./component/Navbar";
-import ReactDOM from 'react-dom/client';
-
+import { Routes,Route } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import PrivateRoute from './route/PrivateRoute';
 //1. 전체 상품페이지, 로그린, 상세상품페이지
 //1-1 네비게이션바를 만든다
 //2. 전체 상품페이지에서는 전체 상품을 볼수있다
@@ -16,18 +17,21 @@ import ReactDOM from 'react-dom/client';
 //6. 로그라웃이 돼면 상품 디테일 페이지를 볼수없다. 다시 로그인 페이지가 보인다
 //7. 로그린을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다
 function App() {
-  return (
-        
-    <div >
-      <Navbar />
-     
-      <Routes>
-        <Route path="/" element={<ProductAll/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/product/:id" element={<ProductDetail/>} />
+  const[authenticate,setAuthenticate]=useState(false);
+  useEffect(()=>{
+    console.log("aaaa"+authenticate);
 
+  },[authenticate])
+  return (
+    
+    <div>
+      <Navbar authenticate={authenticate} setAuthenticate={setAuthenticate}/>
+      <Routes>
+       
+        <Route path="/" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login setAuthenticate={setAuthenticate}/>}/>
+        <Route path="/product/:id" element={<PrivateRoute authenticate={authenticate}/>}/>
       </Routes>
-         
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,42 +1,82 @@
-import React from 'react'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-//import { faCoffee } from '@fortawesome/free-regular-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
-
-
-const Navbar = () => {
-
-  const menuList=['여성','Divided','남성','신생아/유아','아동','H&M Home','Sale','지속가능성']
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+const Navbar = ({ authenticate, setAuthenticate }) => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M HOME",
+    "Sale",
+    "지속가능성",
+  ];
+  let [width, setWidth] = useState(0);
+  let navigate = useNavigate();
+  const onCheckEnter = (event) => {
+    if (event.key === "Enter") {
+      navigate(`?q=${event.target.value}`);
+    }
+  };
   return (
     <div>
-      <div>
-      <div className="login-button">
-        < FontAwesomeIcon icon={ faUser } />
-        <div> 로그인 </div>
+      <div className="side-menu" style={{ width: width }}>
+        <button className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className="side-menu-list" id="menu-list">
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
+        </div>
       </div>
+      <div className="nav-header">
+        <div className="burger-menu hide">
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+        </div>
+        {authenticate ? (
+          <div onClick={() => setAuthenticate(false)}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그아웃</span>
+          </div>
+        ) : (
+          <div onClick={() => navigate("/login")}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그인</span>
+          </div>
+        )}
       </div>
 
-      <div className = "nav-section"> 
-      <img width ={100} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHanMHHVItC0SIRrRcE5BJD_L7mj6Pqk9xyQ&s"  />
+      <div className="nav-logo">
+        <Link to="/">
+          <img
+            width={100}
+            src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1999-present.jpg"
+          />
+        </Link>
       </div>
-
-     <div className="menu-area">
-        <ul className="menu-list"> 
-          {menuList.map((menu) =>(<li>{menu}</li>))}
+      <div class="nav-menu-area">
+        <ul className="menu">
+          {menuList.map((menu, index) => (
+            <li>
+              <a href="#" key={index}>
+                {menu}
+              </a>
+            </li>
+          ))}
         </ul>
-        <div >
-          <FontAwesomeIcon icon={ faSearch } />
-          <input className ="input-style" type='text' placeholder=' 검색 '/>
-        </div> 
+
+        <div className="search-box">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" placeholder="제품검색" onKeyPress={onCheckEnter} />
+        </div>
       </div>
+    </div>
+  );
+};
 
-
-    </div>      
-  )
-}
-
-export default Navbar
+export default Navbar;
